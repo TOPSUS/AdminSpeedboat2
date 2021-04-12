7d0c96a59ce1535d23b425d6ab95db5513409411
Devmptr <dmahaputra@student.unud.ac.id>
2021-04-13 03:31:55 +0700

    refactor : dashboard view baru

--- app/src/main/java/com/espeedboat/admin/DashboardFragment.java
+++ app/src/main/java/com/espeedboat/admin/DashboardFragment.java
@@ -10,51 +10,37 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.budiyev.android.circularprogressbar.CircularProgressBar;
 import com.espeedboat.admin.fragment.ReviewFragment;
 import com.espeedboat.admin.interfaces.FinishActivity;
 import com.espeedboat.admin.interfaces.ToolbarTitle;
 import com.espeedboat.admin.utils.Constants;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DashboardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class DashboardFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
+    View view;
+    BarChart barChart;
     ToolbarTitle toolbarTitleCallback;
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DashboardFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DashboardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DashboardFragment newInstance(String param1, String param2) {
-        DashboardFragment fragment = new DashboardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    CircularProgressBar transaksiProgress, ratingProgress;
+    LinearLayout transaksiData;
 
     @Override
     public void onAttach(Context context) {
@@ -65,29 +51,99 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        RelativeLayout rl = view.findViewById(R.id.wrapper_menu_review);
-        rl.setOnClickListener(new View.OnClickListener() {
+        view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        setIds();
+
+        setBarChart();
+
+        transaksiProgress.setProgress(50f);
+        ratingProgress.setProgress(50f);
+
+        for (int i = 0; i < 5; i++) {
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.transaksi_data, null);
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            if (i == 0) {
+                layoutParams.setMargins(getResources().getDimensionPixelSize(R.dimen.margin_24), 0,
+                        getResources().getDimensionPixelSize(R.dimen.margin_8), 0);
+            } else if (i == 4) {
+                layoutParams.setMargins(0, 0,
+                        getResources().getDimensionPixelSize(R.dimen.margin_24), 0);
+            } else {
+                layoutParams.setMargins(0, 0,
+                        getResources().getDimensionPixelSize(R.dimen.margin_8), 0);
+            }
+
+
+            view.setLayoutParams(layoutParams);
+
+            transaksiData.addView(view);
+        }
+
+        return view;
+    }
+
+    private void setIds() {
+        barChart = view.findViewById(R.id.total_chart);
+        transaksiProgress = view.findViewById(R.id.transaksi_progress);
+        ratingProgress = view.findViewById(R.id.rating_progress);
+        transaksiData = view.findViewById(R.id.transaksi_data_horizontal);
+    }
+
+    private void setBarChart() {
+        // Data-data yang akan ditampilkan di Chart
+        ArrayList<BarEntry> dataPendapatan = new ArrayList<>();
+        dataPendapatan.add(new BarEntry(0, 1500000));
+        dataPendapatan.add(new BarEntry(1, 1430000));
+        dataPendapatan.add(new BarEntry(2, 1750000));
+        dataPendapatan.add(new BarEntry(3, 1390000));
+        dataPendapatan.add(new BarEntry(4, 1430000));
+        dataPendapatan.add(new BarEntry(5, 1750000));
+        dataPendapatan.add(new BarEntry(6, 1390000));
+
+        ArrayList<String> dataHari = new ArrayList<>();
+        dataHari.add("Senin");
+        dataHari.add("Selasa");
+        dataHari.add("Rabu");
+        dataHari.add("Kamis");
+        dataHari.add("Jumat");
+        dataHari.add("Sabtu");
+        dataHari.add("Minggu");
+
+        // Pengaturan atribut bar, seperti warna dan lain-lain
+        BarDataSet dataSet = new BarDataSet(dataPendapatan, "Pendapatan");
+        dataSet.setColor(getResources().getColor(R.color.primary_white));
+        dataSet.setDrawValues(false);
+
+
+        // Membuat Bar data yang akan di set ke Chart
+        BarData barData = new BarData(dataSet);
+
+        barChart.getLegend().setEnabled(false);
+        barChart.getDescription().setEnabled(false);
+        barChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        barChart.setData(barData);
+        barChart.animateY(2000);
+        barChart.getAxisRight().setDrawLabels(false);
+        barChart.getAxisLeft().setDrawLabels(false);
+        barChart.getAxisRight().setDrawGridLines(false);
+        barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getXAxis().setValueFormatter(new ValueFormatter() {
             @Override
-            public void onClick(View v) {
-                toolbarTitleCallback.setToolbarTitle("Review");
-                Fragment fragment = new ReviewFragment();
-                FragmentManager fm = getActivity().getSupportFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.content, fragment, Constants.FRAG_MOVE);
-                ft.commit();
+            public String getFormattedValue(float value) {
+                return dataHari.get((int) value);
             }
         });
-        return view;
+        barChart.getXAxis().setTextColor(getResources().getColor(R.color.primary_white));
+        barChart.getXAxis().setAxisLineColor(getResources().getColor(R.color.primary_white));
     }
 }
--- app/src/main/java/com/espeedboat/admin/SplashActivity.java
+++ app/src/main/java/com/espeedboat/admin/SplashActivity.java
@@ -31,6 +31,9 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        startActivity(new Intent(SplashActivity.this, MainActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+
         bindView();
         hideSystemUI(getWindow());
 
